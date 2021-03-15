96adabe4755b58d5ffedf4f72af4174b2d38a20a
cedric.roustan <cedric.roustan@gmail.com>
2021-03-15 06:27:12 +0100

    use graphql

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listIssues } from './graphql/queries';
+import { createIssue as createIssueMutation, deleteIssue as deleteIssueMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+  const [issues, setIssues] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  async function fetchIssues() {
+    const apiData = await API.graphql({ query: listIssues });
+    setIssues(apiData.data.listIssues.items);
+  }
+
+  async function createIssue() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createIssueMutation, variables: { input: formData } });
+    setIssues([ ...issues, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteIssue({ id }) {
+    const newIssuesArray = issues.filter(issue => issue.id !== id);
+    setIssues(newIssuesArray);
+    await API.graphql({ query: deleteIssueMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
+      <h1>My Issues App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Issue name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Issue description"
+        value={formData.description}
+      />
+      <button onClick={createIssue}>Create Issue</button>
+      <div style={{marginBottom: 30}}>
+        {
+          issues.map(issue => (
+            <div key={issue.id || issue.name}>
+              <h2>{issue.name}</h2>
+              <p>{issue.description}</p>
+              <button onClick={() => deleteIssue(issue)}>Delete issue</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
